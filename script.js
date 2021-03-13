e56fb1ed173fdfb20c75bfc56a2973e91ba15a19
Dinar <difaz2006@yandex.ru>
2021-03-13 15:56:10 +0400

    Это первый комит

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+ //var leftborderwidth = 1;
+//let second = 2;
+//const pi = 3.14;
+//console.log(second); 
+let persone = {
+    name: 'johe',
+    age: 25,
+    isMarried: false
+};
+console.log(persone['name']);
+
+
+console.log ("di" + "nar")
+//alert ('hello world!');
+let anser= prompt ('Есть ли вам 18?', "Да");
+console.log(anser);
